ac6d258ebaa81d41b1bc5a79070a09e0a351deb0
Bobby Martin <robert.martin.619@gmail.com>
2018-09-20 11:45:24 -0700

    Add initial express webApi setup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,25 @@
 import * as functions from 'firebase-functions';
 
+import * as admin from 'firebase-admin';
+import * as firebaseHelper from 'firebase-functions-helper';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+const app = express();
+const main = express();
+
+const phoneNumbersCollection = 'numbers';
+
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+export const webApi = functions.https.onRequest(main);
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
